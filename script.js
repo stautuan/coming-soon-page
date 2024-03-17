c3a87afd70630bd172ba34a337ec36b816ef4f39
stautuan <stautuan89@gmail.com>
2024-03-17 19:36:12 -0400

    Implemented email validation and error handling in JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+"use strict";
 
+const email = document.querySelector(".email");
+const btn = document.querySelector(".btn");
+const errorMessage = document.querySelector(".error-message");
+
+// check email validity
+function isEmailValid(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!isEmailValid(email.value)) {
+    errorMessage.classList.remove("hidden");
+    email.classList.add("highlight-field");
+  } else {
+    errorMessage.classList.add("hidden");
+    email.classList.remove("highlight-field");
+    email.value = "";
+  }
+});
+
+// remove highlight/error message when keys are being input
+email.addEventListener("input", function () {
+  email.classList.remove("highlight-field");
+  errorMessage.classList.add("hidden");
+});
